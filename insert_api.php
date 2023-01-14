46678f98e48a7fe29140ac0ec5a1859285f5d2db
samreen karim <iamsamreenk@gmail.com>
2023-01-14 13:55:02 +0500

    Insert Record-API with UI completed

--- insert_api.php
+++ insert_api.php
@@ -1,40 +1,29 @@
 <?php 
 include 'connect.php';
-
-if(isset($_REQUEST['submit']) || $_SERVER["REQUEST_METHOD"] == 'POST') {
        
-   header('Content-Type: application/json');
-   $data = json_decode(file_get_contents("php://input"), true);        //reading data in json
+    header('Access-Control-Allow-Origin: *');
+    header("Content-Type: application/json; charset=UTF-8");
+    $data = json_decode(file_get_contents("php://input"), true);        //reading data in json
 
-   if(empty($data)){
-    // $extra = array(
-        $title = $_POST['title'];
-        $fname = $_POST['first_name'];
-        $lname = $_POST['last_name'];
-        $gender = $_POST['gender'];
-        $address =  $_POST['address'];
-        $address2 =  $_POST['address2'];
-        $city = $_POST['city'];
-        $state = $_POST['state'];
-        $zip = $_POST['zip'];
-        $textarea = $_POST['textarea'];
-    // );
-    //    $data[] = $extra;
-    //    json_encode($data);
+
+        $title = $data['title'];
+        $fname = $data['first_name'];
+        $lname = $data['last_name'];
+        $gender = $data['gender'];
+        $address =  $data['address'];
+        $address2 =  $data['address2'];
+        $city = $data['city'];
+        $state = $data['state'];
+        $zip = $data['zip'];
+        $textarea = $data['textarea'];
+  
        $sql = "INSERT INTO task (title, first_name, last_name, gender, address, address2, city, state, zip, textarea) 
        VALUES ('$title', '$fname', '$lname', '$gender', '$address', '$address2', '$city', '$state', '$zip', '$textarea')";  
        $res = mysqli_query($connection, $sql);
        
        if($res){
         echo json_encode(array('message' => 'Record Inserted.', 'status' => true));
-        header("Location: insert_form.php");
        }else{
            echo json_encode(array('message' => 'No record found.', 'status' => false));
        }
-   }else{
-    echo "Not inserted";
-   }
-
-}
 ?>
-
--- insert_form.php
+++ insert_form.php
@@ -2,13 +2,16 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-social/bootstrap-social.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
 </head>
 
 <body>
@@ -18,7 +21,7 @@
             <h1 style="text-align: center;">Civil Aviation Form</h1>
         </div><br>
         <div>
-            <form method="post">
+            <form method="post" id="insert_form" name="insert_form">
                 <h4>Personal Information</h4>
                 <div class="form-row">
                     <div class="form-group col-md-2">
@@ -33,11 +36,13 @@
 
                     <div class="form-group col-md-5">
                         <label for="first_name">First Name</label>
-                        <input type="text" class="form-control" id="first_name" name="first_name" required placeholder="e.g. Samreen">
+                        <input type="text" class="form-control" id="first_name" name="first_name" required
+                            placeholder="e.g. Samreen">
                     </div>
                     <div class="form-group col-md-5">
                         <label for="last_name">Last Name</label>
-                        <input type="text" class="form-control" id="last_name" name="last_name" required placeholder="e.g. Karim">
+                        <input type="text" class="form-control" id="last_name" name="last_name" required
+                            placeholder="e.g. Karim">
                     </div>
                 </div>
                 <div class="form-group mx-2">
@@ -46,8 +51,8 @@
                             <legend class="col-form-label col-sm-2 pt-0">Select Gender</legend>
                             <div class="col-sm-10">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" required name="gender" id="male" value="Male"
-                                        checked>
+                                    <input class="form-check-input" type="radio" required name="gender" id="male"
+                                        value="Male" checked>
                                     <label class="form-check-label" for="male">
                                         Male
                                     </label>
@@ -65,7 +70,8 @@
                 </div>
                 <div class="form-group">
                     <label for="address">Address</label>
-                    <input type="text" class="form-control" required name="address" id="address" placeholder="e.g. 1234 Main St">
+                    <input type="text" class="form-control" required name="address" id="address"
+                        placeholder="e.g. 1234 Main St">
                 </div>
                 <div class="form-group">
                     <label for="address2">Address 2</label>
@@ -75,7 +81,8 @@
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="city">City</label>
-                        <input type="text" class="form-control" required id="city" name="city" placeholder="e.g. Islamabad">
+                        <input type="text" class="form-control" required id="city" name="city"
+                            placeholder="e.g. Islamabad">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="state">State</label>
@@ -98,14 +105,19 @@
                     <textarea class="form-control" required id="textarea" rows="3" placeholder="e.g. Designer"
                         name="textarea"></textarea>
                 </div>
-                <input type="submit" name="submit" class="btn btn-primary col-md-2 col-md-offset-10" value="Submit Application" />
+                <input type="submit" id="submit" name="submit" class="btn btn-primary col-md-2 col-md-offset-10"
+                    value="Submit Application" />
             </form>
         </div><br>
 
     </div>
 </body>
 
+
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+</script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
     integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
 </script>
@@ -116,5 +128,35 @@
     integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
 </script>
 
+<script src="jquery.js"></script>
+<script>
+$(document).ready(function() {
+
+    /***** 3rd Route.....Insert Records ******/
+    function converting_toJSON(targetForm) {
+        var arr = $(targetForm).serializeArray();
+        var obj = {};
+        for (var i = 0; i < arr.length; i++) {
+            obj[arr[i].name] = arr[i].value;
+        }
+        var json_string = JSON.stringify(obj);
+        return json_string;
+    }
+
+    $("#submit").on("click", function(e) {
+        e.preventDefault();
+        var json_obj = converting_toJSON("#insert_form");
+        $.ajax({
+            url: "http://localhost/MyWork/API_Task/insert_api.php",
+            type: "POST",
+            data: json_obj,
+            success: function(data) {
+                alert("Record Inserted");
+                 console.log(data);
+            }
+        });
+    });
+});
+</script>
 
 </html>
